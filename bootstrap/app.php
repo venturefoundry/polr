4522c105c0dce93e44c3259b121136033ba416b0
Naitik Patel <naitik.patel@creditmate.in>
2019-12-12 15:27:15 +0530

    changing app url based on base url of project for multiple domain

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -62,6 +62,7 @@ $app->middleware([
     Illuminate\Session\Middleware\StartSession::class,
     Illuminate\View\Middleware\ShareErrorsFromSession::class,
     App\Http\Middleware\VerifyCsrfToken::class,
+    App\Http\Middleware\ChangeAppAddress::class,
 ]);
 
 $app->routeMiddleware([
